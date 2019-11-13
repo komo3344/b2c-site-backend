65b031025502d36e8ec7f73dd75530aab8dc47be
komo3344 <jy940608@naver.com>
2019-11-13 18:04:15 +0900

    add directory media

--- backend/settings.py
+++ backend/settings.py
@@ -131,6 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
