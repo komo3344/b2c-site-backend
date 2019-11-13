7fc3d5390bcffd856c55f3b47b28657863a43701
komo3344 <jy940608@naver.com>
2019-11-13 14:26:25 +0900

    media file code update

--- app/urls.py
+++ app/urls.py
@@ -34,5 +34,7 @@ urlpatterns = [
 
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
